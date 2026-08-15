4a544a14ea43c235280a3a87afc14890ece36391
pandit-pradeep <pp8379951@gmail.com>
2026-08-16 00:39:35 +0530

    Done

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,44 @@
+import Navbar from "./components/Navbar";
+// import ParticleBackground from "./components/ParticlesBackground"
+import CustomCursor from "./components/CustomCursor";
+import Home from "./Section/Home"
+import About from "./Section/About"
+import Contact from "./Section/Contact"
+import Skills from "./Section/Skills"
+import Footer from "./Section/Footer"
+import Projects from "./Section/Projects"
+import Testimonials from "./Section/Testimonials"
+import IntroAnimation from "./components/IntroAnimation";
+import React from "react";
+
+
+
+
+export default function App() {
+
+  const [introDone, setIntroDone] = React.useState(false)
+
+  return (
+    <>
+      {!introDone && <IntroAnimation onFinish={() => setIntroDone(true)} />}
+
+      {introDone && (
+        <div className="relative min-h-screen gradient text-white">
+          <CustomCursor />
+          {/* <ParticleBackground />   */}
+          <Navbar />
+          <Home />
+          <About />
+          <Skills />
+          <Projects />
+          <Testimonials />
+          <Contact />
+          <Footer />
+          {/* <IntroAnimation /> */}
+
+        </div>
+      )}
+    </>
+  )
+
+}
